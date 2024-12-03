445a3a8033c0f60ba50cd1969d9b473c7a4ca9e4
CJ <cjeapel@gmail.com>
2024-12-03 15:23:33 -0500

    3/2

--- 3/main.js
+++ 3/main.js
@@ -6,6 +6,8 @@ const TOKENS = {
   RPAREN: 3,
   COMMA: 4,
   DIGITS: 5,
+  DO: 6,
+  DONT: 7,
 };
 
 function readInput() {
@@ -47,6 +49,12 @@ function parseToken(raw, pos, tok) {
       while (isDigit(raw[idx])) ++idx;
 
       return raw.slice(pos, idx);
+    }
+    case TOKENS.DO: {
+      return raw.slice(pos, pos + 4) === "do()" ? "do()" : false;
+    }
+    case TOKENS.DONT: {
+      return raw.slice(pos, pos + 7) === "don't()" ? "don't()" : false;
     }
   }
 }
@@ -95,9 +103,77 @@ function sumMultiplications(raw) {
   return total;
 }
 
+function sumMultiplicationsDo(raw) {
+  const { MUL, LPAREN, RPAREN, DIGITS, COMMA, DO, DONT } = TOKENS;
+  let total = 0;
+  let on = true;
+  let left = undefined;
+  let right = undefined;
+  let pos = 0;
+  const p = (tok) => parseToken(raw, pos, tok);
+
+  while (pos < raw.length) {
+    if (!on) {
+      const turnOn = p(DO);
+      if (turnOn === false) {
+        pos++;
+        continue;
+      }
+      pos += turnOn.length;
+      on = true;
+    }
+
+    const turnOff = p(DONT);
+    if (turnOff !== false) {
+      pos += turnOff.length;
+      on = false;
+      continue;
+    }
+
+    const mul = p(MUL);
+    if (mul === false) {
+      pos++;
+      continue;
+    }
+    pos += mul.length;
+
+    const lpar = p(LPAREN);
+    if (lpar === false) continue;
+    pos += lpar.length;
+
+    const l = p(DIGITS);
+    if (l === false) continue;
+    pos += l.length;
+    left = Number(l);
+
+    const comma = p(COMMA);
+    if (comma === false) continue;
+    pos += comma.length;
+
+    const r = p(DIGITS);
+    if (r === false) continue;
+    pos += r.length;
+    right = Number(r);
+
+    const rpar = p(RPAREN);
+    if (rpar === false) continue;
+    pos += rpar.length;
+
+    total += left * right;
+  }
+
+  return total;
+}
+
 function solve() {
   const input = readInput();
   return sumMultiplications(input);
 }
 
+function solve2() {
+  const input = readInput();
+  return sumMultiplicationsDo(input);
+}
+
 console.log(solve());
+console.log(solve2());
