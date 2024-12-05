69bc6e9980554fbb4c8c00d4396ae4ea5de00af3
CJ <cjeapel@gmail.com>
2024-12-05 13:34:16 -0500

    5/1

---
+++ 5/main.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+
+function parsePageOrders(rawPageOrders) {
+  const PAGE_ORDER_SEPARATOR = ",";
+  const parse = (order) => order.split(PAGE_ORDER_SEPARATOR);
+
+  return rawPageOrders.map(parse);
+}
+
+function parseDependencies(rawDependencies) {
+  const ret = {};
+  const list = (from) => ret[from] || (ret[from] = []);
+
+  const DEPENDENCY_SEPARATOR = "|";
+  for (const raw of rawDependencies) {
+    const [from, to] = raw.split(DEPENDENCY_SEPARATOR);
+    list(from).push(to);
+  }
+
+  return ret;
+}
+
+function parseInput() {
+  const fd = "input.txt";
+  const raw = fs.readFileSync(fd).toString().split("\n");
+
+  const rawDependencies = [];
+  const rawPageOrders = [];
+
+  let i;
+  for (i = 0; raw[i].trim().length > 0; ++i) {
+    rawDependencies.push(raw[i]);
+  }
+  while (raw[++i].trim().length === 0) {}
+  for (; raw[i].trim().length > 0; ++i) {
+    rawPageOrders.push(raw[i]);
+  }
+
+  return {
+    dependencies: parseDependencies(rawDependencies),
+    pageOrders: parsePageOrders(rawPageOrders),
+  };
+}
+
+function valid(dependencies, order) {
+  const mustPrecede = (a, b) => (dependencies[a] || []).includes(b);
+  for (let right = order.length - 1; right > 0; --right) {
+    for (let left = right - 1; left >= 0; --left) {
+      if (mustPrecede(order[right], order[left])) return false;
+    }
+  }
+
+  return true;
+}
+
+function sumMiddlesOfValidPageOrders(dependencies, pageOrders) {
+  const isValid = valid.bind(null, dependencies);
+  const middle = (arr) => arr[Math.floor(arr.length / 2)];
+  const sum = (a, b) => Number(a) + Number(b);
+
+  return pageOrders.filter(isValid).map(middle).reduce(sum);
+}
+
+function solve() {
+  const { dependencies, pageOrders } = parseInput();
+  return sumMiddlesOfValidPageOrders(dependencies, pageOrders);
+}
+
+console.log(solve());
