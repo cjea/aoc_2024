696a6e981207ef443dda36b5b17817a70359686b
CJ <cjeapel@gmail.com>
2024-12-07 14:34:34 -0500

    7/2

--- 7/main.js
+++ 7/main.js
@@ -15,29 +15,41 @@ function readInput() {
   return map;
 }
 
-function canSolve(nums, target) {
-  const queue = [{ used: 1, total: nums[0] }];
+function plus(n) {
+  return { val: n, apply: (a) => a + n };
+}
+function mult(n) {
+  return { val: n, apply: (a) => a * n };
+}
+function conc(n) {
+  return { val: n, apply: (a) => Number("" + a + n) };
+}
 
+function canSolve(nums, ops, target) {
+  const queue = [{ used: 1, total: nums[0] }];
   while (queue.length) {
     const { used, total } = queue.shift();
     if (total > target) continue;
     if (total === target && used === nums.length) return true;
 
     if (used < nums.length) {
-      queue.push({ used: used + 1, total: total + nums[used] });
-      queue.push({ used: used + 1, total: total * nums[used] });
+      const apply = (op) => op(nums[used]).apply(total);
+      for (const newTotal of ops.map(apply)) {
+        queue.push({ total: newTotal, used: used + 1 });
+      }
     }
   }
 
   return false;
 }
 
-function sumSolvableEquations(input) {
+function sumSolvableEquations(input, ops) {
   let total = 0;
+
   for (const key of Object.keys(input)) {
     const nums = input[key];
     const target = Number(key);
-    if (canSolve(nums, target)) total += target;
+    if (canSolve(nums, ops, target)) total += target;
   }
 
   return total;
@@ -45,9 +57,17 @@ function sumSolvableEquations(input) {
 
 function solve() {
   const input = readInput();
-  const total = sumSolvableEquations(input);
+  const total = sumSolvableEquations(input, [plus, mult]);
+
+  return total;
+}
+
+function solve2() {
+  const input = readInput();
+  const total = sumSolvableEquations(input, [plus, mult, conc]);
 
   return total;
 }
 
 console.log(solve());
+console.log(solve2());
