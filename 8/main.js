9cf8d86219bf19c2e1725964c4b30d2b7515cb2f
CJ <cjeapel@gmail.com>
2024-12-09 21:31:53 -0800

    8/2

--- 8/main.js
+++ 8/main.js
@@ -16,6 +16,27 @@ function move(grid, pos, step) {
   return [r, c];
 }
 
+function walkHarmonic(grid, node, otherNode, onFound) {
+  const delta = ([r1, c1], [r2, c2]) => [r2 - r1, c2 - c1];
+
+  let step;
+  let pos;
+
+  step = delta(node, otherNode);
+  pos = node;
+  while (pos) {
+    onFound(pos.join(","));
+    pos = move(grid, pos, step);
+  }
+
+  step = delta(otherNode, node);
+  pos = otherNode;
+  while (pos) {
+    onFound(pos.join(","));
+    pos = move(grid, pos, step);
+  }
+}
+
 function walk(grid, node, otherNode, onFound) {
   const delta = ([r1, c1], [r2, c2]) => [r2 - r1, c2 - c1];
 
@@ -37,7 +58,7 @@ function walk(grid, node, otherNode, onFound) {
   if (pos) onFound(pos.join(","));
 }
 
-function scanFrequency(grid, frequency, antinodeMap) {
+function scanFrequency(grid, frequency, antinodeMap, antinodeMapHarmonics) {
   const skip = (freq) => freq !== frequency || freq === "." || freq === "#";
   const indexes = new Set();
 
@@ -53,29 +74,35 @@ function scanFrequency(grid, frequency, antinodeMap) {
 
   const decode = (key) => key.split(",").map(Number);
   const addAntinode = (key) => antinodeMap.add(key);
+  const addHarmonicAntinode = (key) => antinodeMapHarmonics.add(key);
 
   const arr = [...indexes].map(decode);
   for (let i = 0; i < arr.length - 1; ++i) {
     for (let j = i + 1; j < arr.length; ++j) {
       walk(grid, arr[i], arr[j], addAntinode);
+      walkHarmonic(grid, arr[i], arr[j], addHarmonicAntinode);
     }
   }
 }
 
 function countAntinodes(grid) {
   const antinodeMap = new Set();
+  const antinodeMapHarmonics = new Set();
   const frequenciesDone = new Set();
   for (let row = 0; row < grid.length; ++row) {
     for (let col = 0; col < grid[row].length; ++col) {
       const frequency = grid[row][col];
       if (frequenciesDone.has(frequency)) continue;
 
-      scanFrequency(grid, frequency, antinodeMap);
+      scanFrequency(grid, frequency, antinodeMap, antinodeMapHarmonics);
       frequenciesDone.add(frequency);
     }
   }
 
-  return antinodeMap.size;
+  return {
+    antinodes: antinodeMap.size,
+    antinodeHarmonics: antinodeMapHarmonics.size,
+  };
 }
 
 function solve() {
