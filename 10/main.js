ce2fb97a5440fbd8e47bae1713c5d8cb30d44698
CJ <cjeapel@gmail.com>
2024-12-12 11:41:20 -0800

    10/2

--- 10/main.js
+++ 10/main.js
@@ -18,6 +18,7 @@ const vectors = [
   [0, 1],
   [1, 0],
 ];
+
 function addVec([r1, c1], [r2, c2]) {
   return [r1 + r2, c1 + c2];
 }
@@ -28,31 +29,48 @@ function getCoords(grid, [row, col]) {
   return undefined;
 }
 
-function score(grid, coords) {
-  const ends = new Set();
-  const seen = (coords) => ends.add(coords.join(","));
-  const distinctEnds = () => ends.size;
+function nextTarget(grid, coords) {
+  return getCoords(grid, coords) + 1;
+}
 
-  const nextTarget = (coords) => getCoords(grid, coords) + 1;
-  const neighbors = (coords) => {
-    const target = nextTarget(coords);
-    return vectors
-      .map((vec) => addVec(vec, coords))
-      .filter((neighbor) => getCoords(grid, neighbor) === target);
-  };
+function neighbors(grid, coords) {
+  const target = nextTarget(grid, coords);
+  return vectors
+    .map((vec) => addVec(vec, coords))
+    .filter((neighbor) => getCoords(grid, neighbor) === target);
+}
 
+function* walk(grid, coords) {
   const queue = [coords];
   while (queue.length) {
     const cur = queue.shift();
     const char = getCoords(grid, cur);
     if (isTrailEnd(char)) {
-      seen(cur);
+      yield cur;
       continue;
     }
-    neighbors(cur).forEach((c) => queue.push(c));
+    neighbors(grid, cur).forEach((c) => queue.push(c));
+  }
+}
+
+function score(grid, coords) {
+  const ends = new Set();
+
+  for (const coord of walk(grid, coords)) {
+    ends.add(coord.join(","));
   }
 
-  return distinctEnds();
+  return ends.size;
+}
+
+function rate(grid, coords) {
+  let total = 0;
+
+  for (const coord of walk(grid, coords)) {
+    total += 1;
+  }
+
+  return total;
 }
 
 function sumTrailScores(input) {
@@ -69,6 +87,20 @@ function sumTrailScores(input) {
   return total;
 }
 
+function sumTrailRatings(input) {
+  let total = 0;
+  for (let row = 0; row < input.length; ++row) {
+    for (let col = 0; col < input[row].length; ++col) {
+      const coords = [row, col];
+      if (!isTrailHead(getCoords(input, coords))) continue;
+
+      total += rate(input, coords);
+    }
+  }
+
+  return total;
+}
+
 function solve() {
   const input = readInput();
   const n = sumTrailScores(input);
@@ -76,4 +108,12 @@ function solve() {
   return n;
 }
 
+function solve2() {
+  const input = readInput();
+  const n = sumTrailRatings(input);
+
+  return n;
+}
+
 console.log(solve());
+console.log(solve2());
