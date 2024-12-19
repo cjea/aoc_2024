24d16f5f0b7c96841ae96b1be71ee02195ef472b
CJ <cjeapel@gmail.com>
2024-12-18 19:35:49 -0800

    14/2

--- 14/main.js
+++ 14/main.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const readline = require("readline");
 
 const fd = "input.txt";
 const COLUMNS = 101;
@@ -30,32 +31,115 @@ function elapse(seconds, { rows, cols, position, velocity }) {
   return [(x + cols) % cols, (y + rows) % rows];
 }
 
-function score(input, seconds, rows = ROWS, cols = COLUMNS) {
-  const middleRow = Math.floor(rows / 2);
-  const middleCol = Math.floor(cols / 2);
-  const factors = [0, 0, 0, 0];
+function tick(input, rows = ROWS, cols = COLUMNS) {
+  for (let i = 0; i < input.length; ++i) {
+    const [position, velocity] = input[i];
+    input[i][0] = elapse(1, { rows, cols, position, velocity });
+  }
+}
 
-  for (const [position, velocity] of input) {
-    const [x, y] = elapse(seconds, { rows, cols, position, velocity });
-    if (x < middleCol) {
-      if (y < middleRow) factors[0] += 1;
-      if (y > middleRow) factors[1] += 1;
+function neighbors(board, row, col) {
+  let neighborRobots = [];
+  const indexable = (o) => (i) => 0 <= i && i < o.length;
+  for (const r of [row - 1, row, row + 1].filter(indexable(board))) {
+    for (const c of [col - 1, col, col + 1].filter(indexable(board[r]))) {
+      if (r === row && c === col) continue;
+
+      if (board[r][c]) neighborRobots.push([r, c]);
     }
-    if (x > middleCol) {
-      if (y < middleRow) factors[2] += 1;
-      if (y > middleRow) factors[3] += 1;
+  }
+  return neighborRobots;
+}
+
+const TOTAL_ROBOTS = 500;
+const NEIGHBOR_THRESHOLD = 2;
+const CONNECTED_FACTOR = 0.5;
+const MIN_CONNECTED = CONNECTED_FACTOR * TOTAL_ROBOTS;
+
+function runAnalysis(board) {
+  let connected = new Set();
+  for (let row = 0; row < board.length; ++row) {
+    for (let col = 0; col < board[row].length; ++col) {
+      if (!board[row][col]) continue;
+
+      const ns = neighbors(board, row, col);
+      if (NEIGHBOR_THRESHOLD <= ns.length) connected.add(`${row},${col}`);
     }
   }
 
-  return factors.reduce((acc, cur) => acc * cur);
+  return {
+    connected: connected.size,
+    total: TOTAL_ROBOTS,
+    ratio: connected.size / TOTAL_ROBOTS,
+  };
+}
+
+function draw(input) {
+  const newRow = () => new Array(COLUMNS).fill(0);
+  const board = new Array(ROWS).fill(null).map(newRow);
+
+  for (const [[x, y]] of input) board[y][x] += 1;
+
+  const analysis = runAnalysis(board);
+  if (analysis.connected < MIN_CONNECTED) {
+    return { ok: false, result: analysis };
+  }
+
+  console.log(board.map((row) => row.map((c) => c || ".").join("")).join("\n"));
+  console.log(analysis);
+
+  return { ok: true, result: analysis };
+}
+
+function quadrants(input, rows = ROWS, cols = COLUMNS) {
+  const middleRow = Math.floor(rows / 2);
+  const middleCol = Math.floor(cols / 2);
+  const quads = [0, 0, 0, 0];
+
+  for (const [[x, y]] of input) {
+    if (x < middleCol) {
+      if (y < middleRow) quads[0] += 1;
+      if (y > middleRow) quads[1] += 1;
+    }
+    if (x > middleCol) {
+      if (y < middleRow) quads[2] += 1;
+      if (y > middleRow) quads[3] += 1;
+    }
+  }
+
+  return quads;
+}
+
+function safetyScore(input, seconds, rows = ROWS, cols = COLUMNS) {
+  for (let i = 0; i < seconds; ++i) tick(input, rows, cols);
+
+  return quadrants(input, rows, cols).reduce((acc, cur) => acc * cur);
 }
 
 function solve() {
   const input = readInput();
   const seconds = 100;
-  const safetyScore = score(input, seconds);
+  const score = safetyScore(input, seconds);
 
-  return safetyScore;
+  return score;
 }
 
-console.log(solve());
+async function solve2() {
+  const input = readInput();
+  const sleep = (n) => new Promise((r) => setTimeout(r, n));
+
+  let seconds = 0;
+  while (true) {
+    tick(input, ROWS, COLUMNS);
+    ++seconds;
+
+    const { ok } = draw(input);
+    if (ok) {
+      console.log({ seconds });
+      await sleep(1000);
+    }
+  }
+}
+
+console.log("Part 1: " + solve());
+solve2();
